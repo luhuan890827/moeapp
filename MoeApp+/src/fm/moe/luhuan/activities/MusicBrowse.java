127d41d2d7a58d4d3ae219107eca29e620861029
luhuan <luhuan890827@gmail.com>
2012-12-11 17:58:12 +0800

    browser almost done!!!

--- MoeApp+/src/fm/moe/luhuan/activities/MusicBrowse.java
+++ MoeApp+/src/fm/moe/luhuan/activities/MusicBrowse.java
@@ -35,6 +35,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.HeaderViewListAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -47,7 +48,8 @@ public class MusicBrowse extends Activity {
 	private ViewPager mViewPager;
 	private Status vStatus = new Status();
 	private LayoutInflater inflater;
-
+	private LinearLayout loadMoreBtn;
+	private LinearLayout loadingProgress ;
 	private MoeOauth http;
 	// private Object lock = new Object();
 	private OnItemClickListener onMainCataClick = new OnMainCataClick();
@@ -61,6 +63,11 @@ public class MusicBrowse extends Activity {
 		setTheme(android.R.style.Theme_Holo);
 		setContentView(R.layout.music_browse);
 		inflater = LayoutInflater.from(this);
+		loadingProgress = (LinearLayout) inflater.inflate(
+				R.layout.progress_view, null);
+		loadMoreBtn = (LinearLayout) inflater.inflate(R.layout.load_more_view, null);
+		loadMoreBtn.setOnClickListener(onLoadMoreBtnClick);
+		
 		// mainWrapper = (RelativeLayout) findViewById(R.id.main_wrapper);
 		mViewPager = (ViewPager) findViewById(R.id.view_pager);
 
@@ -92,10 +99,10 @@ public class MusicBrowse extends Activity {
 		Stack<AdapterDataSet> stack = vStatus.adapterDatas[mViewPager
 				.getCurrentItem()];
 		ListView lv = (ListView) vStatus.views.get(mViewPager.getCurrentItem());
-		clearHintView();
-		
+		lv.removeFooterView(loadingProgress);
+		lv.removeFooterView(loadMoreBtn);
+		//clearHintView();
 		AdapterDataSet dataset = stack.pop();
-		;
 		// Log.e("adapter", ""+adapter);
 		dataset.task.cancel(true);
 		lv.setAdapter(dataset.adapter);
@@ -146,7 +153,7 @@ public class MusicBrowse extends Activity {
 		List<SimpleData> musics = JSONUtils.getSimpelDataList(json, "musics");
 		List<SimpleData> hotMusics = JSONUtils.getSimpelDataList(json,
 				"hot_musics");
-
+		
 		vStatus.datas.put("newAlbums", newAlbums);
 		vStatus.datas.put("hotRadios", hotRadios);
 		vStatus.datas.put("musics", musics);
@@ -177,7 +184,7 @@ public class MusicBrowse extends Activity {
 		ListView listView = (ListView) ll.findViewById(R.id.wrapped_list);
 		vStatus.views.add(listView);
 		LinearLayout hint = (LinearLayout) ll.findViewById(R.id.hint_layout);
-		vStatus.hintViews.add(hint);
+		//vStatus.hintViews.add(hint);
 		MoeDbHelper dbHelper = new MoeDbHelper(this);
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
 		// ContentValues cv = new ContentValues();
@@ -195,6 +202,7 @@ public class MusicBrowse extends Activity {
 		ListAdapter adapter = new MyCursorAdapter(getApplicationContext(), c,
 				false);
 		// 使用cursoradapter勿调用cursor.close();
+		
 		listView.setAdapter(adapter);
 
 		return ll;
@@ -208,11 +216,13 @@ public class MusicBrowse extends Activity {
 		ListView listView = (ListView) ll.findViewById(R.id.wrapped_list);
 		vStatus.views.add(listView);
 		LinearLayout hint = (LinearLayout) ll.findViewById(R.id.hint_layout);
-		vStatus.hintViews.add(hint);
+		//vStatus.hintViews.add(hint);
 
 		String[] tags = new String[] { "收藏的专辑>>", "收藏的电台>>", "喜欢的歌曲>>" };
 		ListAdapter adapter = new ArrayAdapter<String>(this,
 				R.layout.big_text_item, tags);
+//		LinearLayout footer = (LinearLayout) inflater.inflate(R.layout.footer_view, null);
+//		listView.addFooterView(footer);
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(onMainCataClick);
 
@@ -225,14 +235,20 @@ public class MusicBrowse extends Activity {
 
 		ListView listView = (ListView) ll.findViewById(R.id.wrapped_list);
 		LinearLayout hint = (LinearLayout) ll.findViewById(R.id.hint_layout);
-		vStatus.hintViews.add(hint);
+		//vStatus.hintViews.add(hint);
 		vStatus.views.add(listView);
 		String[] tags = new String[] { "音乐热榜>>>", "精选电台>>>", "魔力播放>>>" };
 		ListAdapter adapter = new ArrayAdapter<String>(this,
 				R.layout.big_text_item, tags);
+		/**/
+//		LinearLayout footer = (LinearLayout) inflater.inflate(R.layout.footer_view, null);
+//		listView.addFooterView(footer);
+		//vStatus.footers.add(footer);
+		
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(onMainCataClick);
-
+		
+		
 		return ll;
 	}
 
@@ -243,7 +259,7 @@ public class MusicBrowse extends Activity {
 		public Stack<AdapterDataSet>[] adapterDatas = new Stack[] {
 				new Stack<AdapterDataSet>(), new Stack<AdapterDataSet>(),
 				new Stack<AdapterDataSet>() };
-
+		public List<LinearLayout> footers = new ArrayList<LinearLayout>();
 	}
 
 	private class AdapterDataSet {
@@ -262,22 +278,25 @@ public class MusicBrowse extends Activity {
 		LinearLayout hint = vStatus.hintViews.get(mViewPager.getCurrentItem());
 		hint.removeAllViews();
 	}
-
+	
 	private class OnWikiItemClick implements OnItemClickListener {
 		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 				long arg3) {
+			((ListView)arg0).removeFooterView(loadMoreBtn);
 			AdapterDataSet set = new AdapterDataSet();
 			String type = (String) arg0.getTag();
 			set.adapter = (ListAdapter) arg0.getAdapter();
 			set.onItemClickListener = arg0.getOnItemClickListener();
 			WikiTask task = new WikiTask();
 			set.task = task;
-
+//			TextView t = new TextView(getApplicationContext());
+//			t.setText("1234");
+			//vStatus.footers.get(0).addView(t);
 			vStatus.adapterDatas[mViewPager.getCurrentItem()].push(set);
-
+			
 			String url = null;
 			url = "http://moe.fm/listen/playlist?api=json&" + type + "="
-					+ arg1.getTag(R.string.item_id) + "&perpage=3";
+					+ arg1.getTag(R.string.item_id) + "&perpage=20";
 
 			task.execute(url, "some data",
 					vStatus.views.get(mViewPager.getCurrentItem()));
@@ -295,11 +314,13 @@ public class MusicBrowse extends Activity {
 			set.onItemClickListener = arg0.getOnItemClickListener();
 			vStatus.adapterDatas[mViewPager.getCurrentItem()].push(set);
 			// use hint
-			ProgressBar pb = (ProgressBar) inflater.inflate(
-					R.layout.progress_view, null);
-			setHintView(pb);
+//			ProgressBar pb = (ProgressBar) inflater.inflate(
+//					R.layout.progress_view, null);
+//			setHintView(pb);
+			
+			
 			// Log.e("main caa", "");
-			arg0.setAdapter(null);
+			
 
 			switch (mViewPager.getCurrentItem()) {
 			case 0:
@@ -367,9 +388,15 @@ public class MusicBrowse extends Activity {
 		}
 
 		private void clickAtMainExplore(final int arg2) {
-
+			
 			AsyncTask task = new AsyncTask<Object, Void, Boolean>() {
-
+				@Override
+				protected void onPreExecute() {
+					// TODO Auto-generated method stub
+					super.onPreExecute();
+					vStatus.views.get(0).addFooterView(loadingProgress);
+					vStatus.views.get(0).setAdapter(null);
+				}
 				@Override
 				protected Boolean doInBackground(Object... params) {
 					if (vStatus.datas.size() == 0) {
@@ -382,7 +409,8 @@ public class MusicBrowse extends Activity {
 				}
 
 				protected void onPostExecute(Boolean success) {
-					clearHintView();
+					
+					vStatus.views.get(0).removeFooterView(loadingProgress);
 					if (success) {
 						switch (arg2) {
 						case 0:
@@ -473,9 +501,9 @@ public class MusicBrowse extends Activity {
 			ListView lv = (ListView) vStatus.views.get(mViewPager
 					.getCurrentItem());
 			// use hint
-			ProgressBar pb = (ProgressBar) inflater.inflate(
-					R.layout.progress_view, null);
-			setHintView(pb);
+			
+			//setHintView(pb);
+			lv.addFooterView(loadingProgress);
 			lv.setAdapter(null);
 			super.onPreExecute();
 
@@ -502,23 +530,39 @@ public class MusicBrowse extends Activity {
 
 			ListView listView = (ListView) result[0];
 			ListAdapter adapter = (ListAdapter) result[1];
-			listView.setAdapter(adapter);
-			clearHintView();
+			
+			listView.removeFooterView(loadingProgress);
+			//clearHintView();
 			if (result[2] != null) {
-				TextView tv = (TextView) inflater.inflate(
-						R.layout.big_text_item, null);
-				tv.setText("加载更多");
-				tv.setOnClickListener(onLoadMoreBtnClick);
-				tv.setTag(R.string.more_btn_url, result[2]);
+				listView.addFooterView(loadMoreBtn);
+				loadMoreBtn.setTag(R.string.more_btn_url, result[2]);
+				//Log.e("!!!!", "!!!!!!");
+//				TextView tv = (TextView) inflater.inflate(
+//						R.layout.big_text_item, null);
+//				tv.setText("加载更多");
+//				tv.setOnClickListener(onLoadMoreBtnClick);
+//				tv.setTag(R.string.more_btn_url, result[2]);
 				
-				setHintView(tv);
+				//setHintView(tv);
 			}
+			listView.setAdapter(adapter);
 			
 		}
 
 	}
 
 	public class FavTask extends AsyncTask<String, Void, Object[]> {
+		@Override
+		protected void onPreExecute() {
+			ListView lv = (ListView) vStatus.views.get(mViewPager
+					.getCurrentItem());
+			// use hint
+			
+			//setHintView(pb);
+			lv.addFooterView(loadingProgress);
+			lv.setAdapter(null);
+			super.onPreExecute();
+		}
 		@Override
 		protected Object[] doInBackground(String... params) {
 			// TODO Auto-generated method stub
@@ -539,6 +583,7 @@ public class MusicBrowse extends Activity {
 			ListView listView = (ListView) vStatus.views.get(mViewPager
 					.getCurrentItem());
 			listView.setAdapter((ListAdapter) result[0]);
+			listView.removeFooterView(loadingProgress);
 			if (((String) result[1]).equals("wiki")) {
 
 				listView.setOnItemClickListener(onWikiClick);
@@ -547,14 +592,14 @@ public class MusicBrowse extends Activity {
 				listView.setOnItemClickListener(onSubClick);
 			}
 			listView.setTag(result[2]);
-			clearHintView();
+			//clearHintView();
 			if (result[3] != null) {
 				TextView tv = (TextView) inflater.inflate(
 						R.layout.big_text_item, null);
 				tv.setText("加载更多");
 				tv.setOnClickListener(onLoadMoreBtnClick);
 
-				setHintView(tv);
+				//setHintView(tv);
 
 			}
 
@@ -577,9 +622,10 @@ public class MusicBrowse extends Activity {
 		protected void onPostExecute(Object[] result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
-			clearHintView();
-			SimpleDataAdapter adapter = (SimpleDataAdapter) vStatus.views.get(
+			//clearHintView();
+			HeaderViewListAdapter hAdapter = (HeaderViewListAdapter)vStatus.views.get(
 					mViewPager.getCurrentItem()).getAdapter();
+			SimpleDataAdapter adapter = (SimpleDataAdapter) hAdapter.getWrappedAdapter();
 			adapter.getData().addAll((List<SimpleData>) result[0]);
 			adapter.notifyDataSetChanged();
 			Log.e("next url", result[1] + "");
@@ -588,10 +634,12 @@ public class MusicBrowse extends Activity {
 						R.layout.big_text_item, null);
 				tv.setText("加载更多");
 				tv.setOnClickListener(onLoadMoreBtnClick);
-				tv.setTag(R.string.more_btn_url, result[1]);
-				setHintView(tv);
-
+				loadMoreBtn.setTag(R.string.more_btn_url, result[1]);
+				//setHintView(tv);
+			}else{
+				vStatus.views.get(mViewPager.getCurrentItem()).removeFooterView(loadMoreBtn);
 			}
+			loadMoreBtn.findViewById(R.id.load_more_progress).setVisibility(View.GONE);
 		}
 
 	}
@@ -599,16 +647,17 @@ public class MusicBrowse extends Activity {
 	private OnClickListener onLoadMoreBtnClick = new OnClickListener() {
 
 		public void onClick(View v) {
-			clearHintView();
-			ProgressBar pb = (ProgressBar) inflater.inflate(
-					R.layout.progress_view, null);
-			setHintView(pb);
+//			clearHintView();
+//			ProgressBar pb = (ProgressBar) inflater.inflate(
+//					R.layout.progress_view, null);
+//			setHintView(pb);
 			// SimpleDataAdapter adapter =
 			// (SimpleDataAdapter)vStatus.views.get(mViewPager.getCurrentItem()).getAdapter();
+			v.findViewById(R.id.load_more_progress).setVisibility(View.VISIBLE);
 			String url = (String) v.getTag(R.string.more_btn_url);
 			loadMoreTask task = new loadMoreTask();
 			task.execute(url);
-
+			
 		}
 	};
 	private Handler mMessageHandler = new Handler() {
